f68e6df5b4114888c3668627b051c13b0f66ff0d
Wilmer Rivera <wilmerbaby@gmail.com>
2022-12-04 18:32:07 -0500

    started dynos on heroku

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
 
-app.get("/", function (req, res) {
+app.get("/notes", function (req, res) {
   res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
