f45374df7d8b01d9c5d85e5e5be44f15cc4e7cc0
Wilmer Rivera <wilmerbaby@gmail.com>
2022-12-04 18:19:34 -0500

    deleted notes and left / for home route in heroku upgraded stack

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
 
-app.get("/notes", function (req, res) {
+app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
